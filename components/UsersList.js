50419244653c0bb98ec32685f9a57360d7323c96
Nggohel <gohelnidhi15898@gmail.com>
2023-12-26 11:19:25 +0530

    add

--- components/UsersList.js
+++ components/UsersList.js
@@ -1,11 +1,23 @@
-import { useMemo } from 'react';
+import { useMemo } from "react";
 
 function UsersList({ users, startingLetter }) {
-  
+  const filteredUsers = useMemo(() => {
+    if (!startingLetter) {
+      // If startingLetter is not provided, return the original users list
+      return users;
+    }
+
+    // Filter users based on the starting letter
+    return users.filter((user) =>
+      user.name.toLowerCase().startsWith(startingLetter.toLowerCase())
+    );
+  }, [users, startingLetter]);
 
   return (
     <ul>
-      <li key={}>{}</li>
+      {filteredUsers.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
     </ul>
   );
 }
